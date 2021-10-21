52bfd25c4d0e8144c747203f6f02f7eb66cfc6fa
unknown <steve.potts@bowlereggs.co.uk>
2021-10-21 18:38:35 +0100

    basic cards and basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlayerTableComponent } from './player-table/player-table.component';
-
+import { BasicCardComponent } from './basic-card/basic-card.component';
 // Mat Mods
-import { MatTableModule } from '@angular/material/table' 
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerTableComponent
+    PlayerTableComponent,
+    BasicCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
