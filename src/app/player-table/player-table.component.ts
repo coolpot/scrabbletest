1e2af739262d2a56cdf5f3734ef6c1e2ea1a76a4
unknown <steve.potts@bowlereggs.co.uk>
2021-10-22 00:19:41 +0100

    sorting fixed

--- src/app/player-table/player-table.component.ts
+++ src/app/player-table/player-table.component.ts
@@ -1,41 +1,44 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { PlayerDataService } from '../player-data.service';
 import { Player } from '../player.model';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-player-table',
   templateUrl: './player-table.component.html',
-  styleUrls: ['./player-table.component.scss']
+  styleUrls: ['./player-table.component.scss'],
 })
-
-export class PlayerTableComponent implements OnInit, AfterViewInit {
-  displayedColumns: string[] = ['PlayerId', 'Name', 'GamesPlayed', 'TotalScore'];
-  dataSource = new MatTableDataSource<Player>();
-  
+export class PlayerTableComponent implements OnInit {
+  displayedColumns: string[] = [
+    'PlayerId',
+    'Name',
+    'GamesPlayed',
+    'TotalScore',
+  ];
+  @ViewChild(MatSort) sort: MatSort;
   playerData = [];
   gameData = [];
+  playerDataArray = [];
+  dataSource;
 
-  constructor(private playerDataService: PlayerDataService) { }
-  @ViewChild(MatSort, { static: false}) sort: MatSort;
+  constructor(private playerDataService: PlayerDataService) {}
 
-
-  
   ngOnInit(): void {
-    this.playerDataService.getCombinedPlayerData().subscribe(res => {
+    this.playerDataService.getCombinedPlayerData().subscribe((res) => {
       const players = res[0].Players;
       const games = res[1].Results;
-      console.log(players, games);
-      const playerDataArray = players.map(p => ({ ...p, ...games.find(g => g.PlayerId === p.PlayerId) }));
-      this.dataSource = playerDataArray;
+      this.playerDataArray = players.map((p) => ({
+        ...p,
+        ...games.find((g) => g.PlayerId === p.PlayerId),
+      }));
+      console.log(this.playerDataArray);
+      this.dataSource = new MatTableDataSource(this.playerDataArray);
+      this.dataSource.sort = this.sort; 
     });
-    
-
   }
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
   }
-
 }
