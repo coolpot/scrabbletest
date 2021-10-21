727db607e9d548f17b22d0a5a0a47b18b85c310a
unknown <steve.potts@bowlereggs.co.uk>
2021-10-21 18:25:28 +0100

    basic table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PlayerTableComponent } from './player-table/player-table.component';
+
+// Mat Mods
+import { MatTableModule } from '@angular/material/table' 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/player.model.ts
@@ -0,0 +1,6 @@
+export interface Player {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
